8fb11a3c39f15138732f3689d8e09b8227b11b50
Gilberto Ayala <gilbertoayala12@gmail.com>
2017-05-18 15:04:42 -0700

    header :joy:

--- 7.- final project/final.asm
+++ 7.- final project/final.asm
@@ -2,6 +2,18 @@
 ;prints three strings, using functions for repeated tasks
 ;author: Erick Delfin, Gilberto Ayala, German Verdugo
 ;date: 2017/05/11
+; Ejemplo de como se usa
+; Se muestra un menu con las opciones requeridas del proyecto,
+; al seleccionar la opcion 1 te pide el nombre y se guarda la 
+; opcion 2 te pide la calificacion de los alumnos guardados en
+; en caso de volver a seleccionar esta opcion de nuevo despues
+; de ya tener guardadas calificaciones se sobrescribiran con 
+; las nuevas que se introduzcan la opcion, la opcion 3 muestran 
+; las calificaciones, minimo, maximo, promedio y desviacion estandar
+; la opcion 4 te muestra el mensaje que te pide nombre para el archivo
+; a guardar y para leer dicho archivo usar el comando 
+; "$ cat <nombre del archivo>" despliega las calificaciones guardadas
+; la opcion 0 es para salir.
 
 %include '../2.- functions/functions.asm'
 
